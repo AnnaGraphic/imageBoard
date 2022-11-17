c1a7007ef1ef8ceb590a2d11f7634bcc39790a4a
annar <p4nd4@posteo.de>
2022-11-17 18:25:31 +0100

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,33 @@
-import * as Vue from './vue.js';
+// Vue = obj
+import * as Vue from "./vue.js";
+//               where we want to hook HTML
+Vue.createApp({
+    //convention call function 'data'
+    data() {
+        //has to return an obj
+        return {
+            description: "hello world",
+            first: "",
+            count: 0,
+            images[],
+        };
+    },
+    //cconvention: define function in methodsproperty(also obj)
+    methods: {
+        updateFirst: function (e) {
+            //this reffered to obj
+            this.first = "hello";
+        },
+    },
+    // mounted macht, dass die elemente erst geholt werden wenn die seite geladen hat
+    mounted() {
+        fetch("/images")
+            .then((res) => res.json())
+            .then((cities) => {
+                this.cities = cities;
+            });
+    },
+}).mount("#main");
+
+//create endpoint to database
+//create layout
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const { PORT = 8080 } = process.env;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-
+// * no real routing = typically for onepage apps
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
+
+//img-route machen
 
 app.listen(PORT, () => console.log(`I'm listening on port ${PORT}`));
